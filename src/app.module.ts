edd2c0aa851cdebbe7c0801f4e0c8053f582782e
rodrigo15975 <javierluis15975@hotmail.com>
2025-04-06 04:44:14 -0500

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Logger, Module } from '@nestjs/common'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from 'nestjs-prisma'
 import { AwsModule } from './module/aws/aws.module'
 import { AwsService } from './module/aws/aws.service'
-import { ConfigModule } from '@nestjs/config'
 import { UsersModule } from './module/users/users.module'
-import { PrismaModule } from 'nestjs-prisma'
 
 @Module({
   imports: [
@@ -23,17 +23,20 @@ export class AppModule {
   constructor(private readonly awsService: AwsService) {}
 
   async onModuleInit() {
-    const secrets = await this.awsService.getSecretManager(
-      'nestjs-db-credentials',
-    )
-    for (const [key, value] of Object.entries(secrets)) {
-      if (!process.env[key]) {
-        process.env[key] = value
-        Logger.debug('Assigning secret: ' + key + ' = ' + value)
-      } else {
-        Logger.debug('Secret already assigned: ' + key + ' = ' + value)
-        process.env[key] = value
-      }
-    }
+    // const secrets = await this.awsService.getSecretManager(
+    //   'nestjs-db-credentials',
+    // )
+    // console.log({
+    //   secrets,
+    // })
+    // for (const [key, value] of Object.entries(secrets)) {
+    //   if (!process.env[key]) {
+    //     process.env[key] = value
+    //     Logger.debug('Assigning secret: ' + key + ' = ' + value)
+    //   } else {
+    //     Logger.debug('Secret already assigned: ' + key + ' = ' + value)
+    //     process.env[key] = value
+    //   }
+    // }
   }
 }
